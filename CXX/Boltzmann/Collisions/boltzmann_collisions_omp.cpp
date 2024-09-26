53deaa4ff0fe303f37f703c3da549834764e2124
William Sands <wsands@udel.edu>
2024-09-26 07:36:08 -0700

    Project updates for the OpenMP miniapp. Using a large parallel region with barriers to prevent thread creation overhead. Working on a new batched implementation with precomputing optimizations.

--- CXX/Boltzmann/Collisions/boltzmann_collisions_omp.cpp
+++ CXX/Boltzmann/Collisions/boltzmann_collisions_omp.cpp
@@ -74,32 +74,35 @@ void boltzmann_vhs_spectral_solver_omp(std::vector<double> &Q,
     fftw_plan ifft_alpha1_times_f_hat = fftw_plan_dft_3d(Nv, Nv, Nv, 
                                                          reinterpret_cast<fftw_complex*>(alpha1_times_f_hat), 
                                                          reinterpret_cast<fftw_complex*>(alpha1_times_f), 
-                                                         FFTW_BACKWARD, FFTW_EXHAUSTIVE); 
+                                                         FFTW_BACKWARD, FFTW_ESTIMATE); 
 
     fftw_plan ifft_alpha2_times_f_hat = fftw_plan_dft_3d(Nv, Nv, Nv, 
                                                          reinterpret_cast<fftw_complex*>(alpha2_times_f_hat), 
                                                          reinterpret_cast<fftw_complex*>(alpha2_times_f), 
-                                                         FFTW_BACKWARD, FFTW_EXHAUSTIVE); 
+                                                         FFTW_BACKWARD, FFTW_ESTIMATE); 
 
     fftw_plan fft_product = fftw_plan_dft_3d(Nv, Nv, Nv, 
                                              reinterpret_cast<fftw_complex*>(transform_prod), 
                                              reinterpret_cast<fftw_complex*>(transform_prod_hat), 
-                                             FFTW_FORWARD, FFTW_EXHAUSTIVE);
+                                             FFTW_FORWARD, FFTW_ESTIMATE);
 
     fftw_plan ifft_Q_gain_hat = fftw_plan_dft_3d(Nv, Nv, Nv, 
                                                  reinterpret_cast<fftw_complex*>(Q_gain_hat), 
                                                  reinterpret_cast<fftw_complex*>(Q_gain), 
-                                                 FFTW_BACKWARD, FFTW_EXHAUSTIVE);
+                                                 FFTW_BACKWARD, FFTW_ESTIMATE);
 
     fftw_plan ifft_beta2_times_f_hat = fftw_plan_dft_3d(Nv, Nv, Nv, 
                                                         reinterpret_cast<fftw_complex*>(beta2_times_f_hat), 
                                                         reinterpret_cast<fftw_complex*>(beta2_times_f), 
-                                                        FFTW_BACKWARD, FFTW_EXHAUSTIVE); 
+                                                        FFTW_BACKWARD, FFTW_ESTIMATE); 
 
     // Export wisdom immediately after plan creation
     fftw_export_wisdom_to_filename(fname.c_str());
 
-    #pragma omp parallel for collapse(2)
+    #pragma omp parallel
+    {
+
+    #pragma omp for collapse(2)
     for (int i = 0; i < Nv; ++i){
         for (int j = 0; j < Nv; ++j){
             #pragma omp simd
@@ -111,15 +114,17 @@ void boltzmann_vhs_spectral_solver_omp(std::vector<double> &Q,
             }
         }
     }
-    
+
     // Transform f to get f_hat
+    #pragma omp barrier
+    #pragma omp single
     fftw_execute(fft_f);
    
     for (int r = 0; r < Nr; ++r){
         for (int s = 0; s < Ns; ++s){
 
             // Compute the complex weights alpha1 and alpha2 to scale the gain term
-            #pragma omp parallel for collapse(2)
+            #pragma omp for collapse(2)
             for (int i = 0; i < Nv; ++i){
                 for (int j = 0; j < Nv; ++j){
                     #pragma omp simd
@@ -140,11 +145,15 @@ void boltzmann_vhs_spectral_solver_omp(std::vector<double> &Q,
             }
 
             // Invert the weighted transforms of f (normalized prior to execution)
+            #pragma omp barrier
+            #pragma omp single
+            {
             fftw_execute(ifft_alpha1_times_f_hat);
             fftw_execute(ifft_alpha2_times_f_hat);
+            }
 
             // Compute the product of the transforms in physical space
-            #pragma omp parallel for collapse(2)
+            #pragma omp for collapse(2)
             for (int i = 0; i < Nv; ++i){
                 for (int j = 0; j < Nv; ++j){
                     #pragma omp simd
@@ -156,12 +165,14 @@ void boltzmann_vhs_spectral_solver_omp(std::vector<double> &Q,
             }
 
             // Transform the product back to the frequency domain
+            #pragma omp barrier
+            #pragma omp single
             fftw_execute(fft_product);
 
             // Update the gain term in the frequency domain
             // Each term that is added is normalized
             // Note: Since the quadrature loops are done in serial, there is no need for a reduction clause
-            #pragma omp parallel for collapse(2)
+            #pragma omp for collapse(2)
             for (int i = 0; i < Nv; ++i){
                 for (int j = 0; j < Nv; ++j){
                     #pragma omp simd
@@ -172,11 +183,14 @@ void boltzmann_vhs_spectral_solver_omp(std::vector<double> &Q,
                 }
             }
 
+            // Synchronization between radial and spherical loop
+            #pragma omp barrier
+
         } // End of spherical loop
 
         // Compute the complex weights beta2
         // Note: Since the quadrature loops are done in serial, there is no need for a reduction clause
-        #pragma omp parallel for collapse(2)
+        #pragma omp for collapse(2)
         for (int i = 0; i < Nv; ++i){
             for (int j = 0; j < Nv; ++j){
                 #pragma omp simd
@@ -188,10 +202,12 @@ void boltzmann_vhs_spectral_solver_omp(std::vector<double> &Q,
             }
         }
 
+        #pragma omp barrier
+
     } // End of radial loop
 
     // Apply weights beta2 to f_hat and normalize
-    #pragma omp parallel for collapse(2)
+    #pragma omp for collapse(2)
     for (int i = 0; i < Nv; ++i){
         for (int j = 0; j < Nv; ++j){
             #pragma omp simd
@@ -202,16 +218,20 @@ void boltzmann_vhs_spectral_solver_omp(std::vector<double> &Q,
         }
     }
 
+    #pragma omp barrier
+
     // Transform Q_gain back to physical space
     // This needs to be normalized
+    #pragma omp single
     fftw_execute(ifft_Q_gain_hat);
 
     // Transform beta2_times_f_hat back to physical space
     // This also needs to be normalized
+    #pragma omp single
     fftw_execute(ifft_beta2_times_f_hat);
 
     // Compute the final form for Q = real(Q_gain - Q_loss)
-    #pragma omp parallel for collapse(2)
+    #pragma omp for collapse(2)
     for (int i = 0; i < Nv; ++i){
         for (int j = 0; j < Nv; ++j){
             #pragma omp simd
@@ -222,6 +242,8 @@ void boltzmann_vhs_spectral_solver_omp(std::vector<double> &Q,
             }
         }            
     }
+
+    } // End of omp parallel region (implicit barrier)
 
     // Destory the plans for each of the transforms
     fftw_destroy_plan(fft_f);
